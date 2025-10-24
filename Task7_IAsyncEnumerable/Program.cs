dcd5c573521553374710704a2d973ee24dc39ddf
Kirill Zhestkov <infinity.crime@outlook.com>
2025-10-24 15:44:06 +0300

    TASK-305-7: Реализовано использование асинхронного стрима.

---
+++ Task7_IAsyncEnumerable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace Task7_IAsyncEnumerable
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Текущий поток (Main): {Environment.CurrentManagedThreadId}");
+            await foreach(var number in GetNumbersAsync())
+            {
+                Console.WriteLine($"Полученное число из асинхронного стрима: {number}");
+            }
+        }
+
+        /*
+            Первый вызов MoveNextAsync() будет происходить в текущем потоке, затем в
+            ThreadPool. Это можно заметить в выводе информации в консоли о текущем
+            Id потока.
+         */
+        static async IAsyncEnumerable<int> GetNumbersAsync()
+        {
+            for(int i = 0; i < 4; ++i)
+            {
+                Console.WriteLine($"Текущий поток: {Environment.CurrentManagedThreadId}");
+                await Task.Delay(1000);
+                yield return i * 2;
+            }
+        }
+    }
+}
